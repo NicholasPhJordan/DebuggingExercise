8cd7acd64e7000082576c8054153af3e6d7ffa9d
Nicholas Jordan <NicholasPhJordan@gmail.com>
2020-09-04 09:48:16 -0500

    got it to player can choose name and then it speeds through to the end

--- Game.cs
+++ Game.cs
@@ -12,16 +12,94 @@ namespace HelloWorld
         int _playerDamage = 20;
         int _playerDefense = 10;
         int levelScaleMax = 5;
-        //Run the game
-        public void Run()
+
+        //Displays the character selection menu. 
+        void SelectCharacter()
         {
-
-            while(_gameOver == false)
+            char input = ' ';
+            //Loops until a valid option is choosen
+            while (input != '1' && input != '2' && input != '3')
             {
+                //Prints options
+                Console.WriteLine("Welcome! Please select a character.");
+                Console.WriteLine("1.Sir Kibble");
+                Console.WriteLine("2.Gnojoel");
+                Console.WriteLine("3.Joedazz");
+                Console.Write("> ");
+                input = Console.ReadKey().KeyChar;
+                //Sets the players default stats based on which character was picked
+                switch (input)
+                {
+                    case '1':
+                        _playerName = "Sir Kibble";
+                        _playerHealth = 120;
+                        _playerDefense = 10;
+                        _playerDamage = 40;
+                        break;
+                    case '2':
+                        _playerName = "Gnojoel";
+                        _playerHealth = 40;
+                        _playerDefense = 2;
+                        _playerDamage = 70;
+                        break;
+                    case '3':
+                        _playerName = "Joedazz";
+                        _playerHealth = 200;
+                        _playerDefense = 5;
+                        _playerDamage = 25;
+                        break;
+                    //If an invalid input is selected display and input message and input over again.
+                    default:
+                        Console.WriteLine("\nInvalid input. Press any key to continue.");
+                        Console.Write("> ");
+                        Console.ReadKey();
+                        continue;
+                }
+                Console.Clear();
+            }
+            //Prints the stats of the choosen character to the screen before the game begins to give the player visual feedback
+            PrintStats(_playerName, _playerHealth, _playerDamage, _playerDefense);
+            Console.WriteLine("Press any key to continue.");
+            Console.Write("> ");
+            Console.ReadKey();
+            Console.Clear();
+        }
 
+        //This is used to progress through our game. A recursive function meant to switch the rooms and start the battles inside them.
+        void ClimbLadder(int roomNum)
+        {
+            int turnCount = 0;
+
+            //Displays context based on which room the player is in
+            switch (roomNum)
+            {
+                case 0:
+                    Console.WriteLine("A wizard blocks your path!");
+                    StartBattle(roomNum, ref turnCount);
+                    break;
+                case 1:
+                    Console.WriteLine("A troll stands before you!");
+                    StartBattle(roomNum, ref turnCount);
+                    break;
+                case 2:
+                    Console.WriteLine("A giant has appeared!");
+                    StartBattle(roomNum, ref turnCount);
+                    break;
+                default:
+                    _gameOver = true;
+                    return;
             }
 
+            //Starts a battle. If the player survived the battle, level them up and then proceed to the next room.
+            if (_playerHealth > 0)
+            {
+                LevelUp(turnCount += 1);
+                ClimbLadder(roomNum += 1);
+            }
+            _gameOver = true;
+
         }
+
         //This function handles the battles for our ladder. roomNum is used to update the our opponent to be the enemy in the current room. 
         //turnCount is used to keep track of how many turns it took the player to beat the enemy
         bool StartBattle(int roomNum, ref int turnCount)
@@ -36,41 +114,37 @@ namespace HelloWorld
             switch (roomNum)
             {
                 case 0:
-                    {
-                        enemyHealth = 100;
-                        enemyAttack = 20;
-                        enemyDefense = 5;
-                        enemyName = "Wizard";
-                    }
+                    enemyHealth = 100;
+                    enemyAttack = 20;
+                    enemyDefense = 5;
+                    enemyName = "Wizard";
+                    break;
                 case 1:
-                    {
-                        enemyHealth = 80;
-                        enemyAttack = 30;
-                        enemyDefense = 5;
-                        enemyName = "Troll";
-                    }
-                case 2
-                    {
-                        
-                        enemyHealth = 200;
-                        enemyAttack = 40;
-                        enemyDefense = 10;
-                        enemyName = "Giant";
-                    }
+                    enemyHealth = 80;
+                    enemyAttack = 30;
+                    enemyDefense = 5;
+                    enemyName = "Troll";
+                    break;
+                case 2:
+                    enemyHealth = 200;
+                    enemyAttack = 40;
+                    enemyDefense = 10;
+                    enemyName = "Giant";
+                    break;
             }
 
             //Loops until the player or the enemy is dead
-            while(_playerHealth >= 0 || enemyHealth >= 0)
+            while (_playerHealth <= 0 && enemyHealth <= 0)
             {
                 //Displays the stats for both charactersa to the screen before the player takes their turn
                 PrintStats(_playerName, _playerHealth, _playerDamage, _playerDefense);
                 PrintStats(enemyName, enemyHealth, enemyAttack, enemyDefense);
 
                 //Get input from the player
-                char input;
-                GetInput(input, "Attack", "Defend");
+                char input = ' ';
+                input = GetInput("Attack", "Defend");
                 //If input is 1, the player wants to attack. By default the enemy blocks any incoming attack
-                if(input == '1')
+                if (input == '1')
                 {
                     BlockAttack(enemyHealth, _playerDamage, enemyDefense);
                     Console.WriteLine("You dealt " + _playerDamage + " damage.");
@@ -79,7 +153,7 @@ namespace HelloWorld
                 }
                 //If the player decides to defend the enemy just takes their turn. However this time the block attack function is
                 //called instead of simply decrementing the health by the enemy's attack value.
-                else
+                else if (input == '2')
                 {
                     BlockAttack(_playerHealth, enemyAttack, _playerDefense);
                     Console.WriteLine(enemyName + " dealt " + enemyAttack + " damage.");
@@ -95,150 +169,76 @@ namespace HelloWorld
                 Console.Write("> ");
                 Console.ReadKey();
                 turnCount++;
-                
             }
             //Return whether or not our player died
-            return _playerHealth != 0;
-
+            return _playerHealth <= 0;
         }
+
         //Decrements the health of a character. The attack value is subtracted by that character's defense
         void BlockAttack(int opponentHealth, int attackVal, int opponentDefense)
         {
             int damage = attackVal - opponentDefense;
-            if(damage < 0)
+            if (damage < 0)
             {
-                damage = 0;
+                damage = 1;
             }
             opponentHealth -= damage;
         }
+
         //Scales up the player's stats based on the amount of turns it took in the last battle
         void LevelUp(int turnCount)
         {
             //Subtract the amount of turns from our maximum level scale to get our current level scale
             int scale = levelScaleMax - turnCount;
-            if(scale <= 0)
+            if (scale <= 0)
             {
-                scale = 1;
+                scale += 1;
             }
             _playerHealth += 10 * scale;
             _playerDamage *= scale;
             _playerDefense *= scale;
         }
+
         //Gets input from the player
         //Out's the char variable given. This variables stores the player's input choice.
         //The parameters option1 and option 2 displays the players current chpices to the screen
-        void GetInput(out char input,string option1, string option2)
+        char GetInput(string option1, string option2)
         {
             //Initialize input
-            input = ' ';
+            char input = ' ';
             //Loop until the player enters a valid input
-            while(input != '1' && input != '2')
-                Console.WriteLine("1." + option1);
-                Console.WriteLine("2." + option2);
+            while (input != '1' && input != '2')
+            {
+                Console.WriteLine("1. " + option1);
+                Console.WriteLine("2. " + option2);
                 Console.Write("> ");
                 input = Console.ReadKey().KeyChar;
+            }
+            return input;
         }
 
         //Prints the stats given in the parameter list to the console
         void PrintStats(string name, int health, int damage, int defense)
         {
-            Console.WriteLine("/n" + name);
+            Console.WriteLine("Name: " + name);
             Console.WriteLine("Health: " + health);
             Console.WriteLine("Damage: " + damage);
             Console.WriteLine("Defense: " + defense);
         }
 
-        //This is used to progress through our game. A recursive function meant to switch the rooms and start the battles inside them.
-        void ClimbLadder(int roomNum)
+        //Run the game
+        public void Run()
         {
-            //Displays context based on which room the player is in
-            switch (roomNum)
-            {
-                case 0:
-                    {
-                        Console.WriteLine("A wizard blocks your path");
-                    }
-                case 1:
-                    {
-                        Console.WriteLine("A troll stands before you");
-                    }
-                case 2
-                    {
-                        Console.WriteLine("A giant has appeared!");
-                    }
-                default:
-                    {
-                        _gameOver = true;
-                        return;
-                    }
-            }
-            int turnCount = 0;
-            //Starts a battle. If the player survived the battle, level them up and then proceed to the next room.
-            if(StartBattle(roomNum, ref turnCount))
-            {
-                LevelUp(turnCount);
-                ClimbLadder(roomNum++);
-            }
-            _gameOver = true;
+            Start();
 
+            while (_gameOver == false)
+            {
+                Update();
+            }
+
+            End();
         }
 
-        //Displays the character selection menu. 
-        void SelectCharacter()
-        {
-            char input = ' ';
-            //Loops until a valid option is choosen
-            while(input != '1' && input != '2' && input != '3')
-            {
-                //Prints options
-                Console.WriteLine("Welcome! Please select a character.");
-                Console.WriteLine("1.Sir Kibble");
-                Console.WriteLine("2.Gnojoel");
-                Console.WriteLine("3.Joedazz");
-                Console.Write("> ");
-                input = Console.ReadKey().KeyChar;
-                //Sets the players default stats based on which character was picked
-                switch (input)
-                {
-                    case '1':
-                        {
-                            _playerName = "Sir Kibble";
-                            _playerHealth = 120;
-                            _playerDefense = 10;
-                            _playerDamage = 40;
-                        }
-                    case '2':
-                        {
-                            _playerName = "Gnojoel";
-                            _playerHealth = 40;
-                            _playerDefense = 2;
-                            _playerDamage = 70;
-                        }
-                    case '3'
-                        {
-                            _playerName = "Joedazz";
-                            _playerHealth = 200;
-                            _playerDefense = 5;
-                            _playerDamage = 25;
-                        }
-                        //If an invalid input is selected display and input message and input over again.
-                    default:
-                        {
-                            Console.WriteLine("Invalid input. Press any key to continue.");
-                            Console.Write("> ");
-                            Console.ReadKey();
-                            break;
-                        }
-                }
-                Console.Clear();
-            }
-            //Prints the stats of the choosen character to the screen before the game begins to give the player visual feedback
-            PrintStats(_playerName,_playerHealth,_playerDamage,_playerDefense);
-            Console.WriteLine("Press any key to continue.");
-            Console.Write("> ");
-            Console.ReadKey();
-            Console.Clear();
-        }
         //Performed once when the game begins
         public void Start()
         {
@@ -248,14 +248,14 @@ namespace HelloWorld
         //Repeated until the game ends
         public void Update()
         {
-            ClimbLadder(0);   
+            ClimbLadder(0);
         }
 
         //Performed once when the game ends
         public void End()
         {
             //If the player died print death message
-            if(_playerHealth <= 0)
+            if (_playerHealth <= 0)
             {
                 Console.WriteLine("Failure");
                 return;
